4718e10421cd4a9403c458085488979b73569770
Master <Master@DESKTOP-AKG9PLP>
2018-03-05 22:01:56 +0900

    Update using class method

--- src/codesquad/calendar/Calendar.java
+++ src/codesquad/calendar/Calendar.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class Calendar {
 
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysOfMonth(int month){
+		return MAX_DAYS[month -1];
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
@@ -12,12 +18,13 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 
-
+		Calendar cal = new Calendar();
+		
 		System.out.println("달을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
 		int month = scanner.nextInt();
-		int[]maxDays={31,28,31,30,31,30,31,31,30,31,30,31};
-		System.out.printf("%d월은 %d일까지 있습니다.", month,maxDays[month-1]);
+		
+		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
 		scanner.close();
 	}
 
