be713e0d9744a1df58d418ffc24f078deeb3c1cb
Master <Master@DESKTOP-AKG9PLP>
2018-03-07 01:07:41 +0900

    Update calendar using if and while statements.

--- src/codesquad/calendar/Calendar.java
+++ src/codesquad/calendar/Calendar.java
@@ -6,10 +6,10 @@ public class Calendar {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public static int getMaxDaysOfMonth(int month){
-		return MAX_DAYS[month -1];
+	public static int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
@@ -17,17 +17,28 @@ public class Calendar {
 		System.out.println("8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("반복횟수를 입력하세요.");
-		int repeat = scanner.nextInt();
-		System.out.println("달을 입력하세요.");
-		for(int i = 0; i<repeat; i++){
-			int month=scanner.nextInt();
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, Calendar.getMaxDaysOfMonth(month));
 
+		String PROMPT ="cal> ";
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("월을 입력하세요.");
+			System.out.print(PROMPT);
+			int month = scanner.nextInt();
+			
+			if(month>12){
+				continue;
+			}
+			
+			if (month != -1) {
+				System.out.printf("%d월은 %d일까지 있습니다.\n", month, Calendar.getMaxDaysOfMonth(month));
+
+			} else {
+				System.out.println("Have a nice day!");
+				break;
+			}
 		}
-		System.out.println("end!");
+
 		scanner.close();
 	}
 
