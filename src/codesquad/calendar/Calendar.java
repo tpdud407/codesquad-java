386723b4c02046bbd5ed6b86f0cf1b781b478bfc
Master <Master@DESKTOP-AKG9PLP>
2018-03-07 23:22:10 +0900

    Create a virtual calendar and updated.

--- src/codesquad/calendar/Calendar.java
+++ src/codesquad/calendar/Calendar.java
@@ -9,14 +9,24 @@ public class Calendar {
 	public static int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
+	
+	public static void printCalendar(int year,int month){
+		System.out.printf("   <<%4d년%3d월>>\n ",year,month);
+		System.out.println("  일 월 화 수 목 금 토");
+		System.out.println(" --------------------");
+		
+		int maxDay = getMaxDaysOfMonth(month);
+		
+		for(int i =1; i<maxDay; i++){
+			System.out.printf("%3d",i);
+			if(i%7==0){
+				System.out.println();
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		System.out.println("일 월 화 수 목 금 토");
-		System.out.println("--------------------");
-		System.out.println("1  2  3  4  5  6  7");
-		System.out.println("8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+		
 
 		String PROMPT ="cal> ";
 		Scanner scanner = new Scanner(System.in);
@@ -30,10 +40,10 @@ public class Calendar {
 				continue;
 			}
 			
-			if (month != -1) {
-				System.out.printf("%d월은 %d일까지 있습니다.\n", month, Calendar.getMaxDaysOfMonth(month));
-
-			} else {
+			if(month != -1){
+				Calendar.printCalendar(2017, month);
+				System.out.println();
+			}else{
 				System.out.println("Have a nice day!");
 				break;
 			}
